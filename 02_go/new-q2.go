343b5de38d887f5082df951ace036733ac028800
vdcn1 <veetsirrah@gmail.com>
2020-08-30 22:48:11 -0300

    Versão 1 da q2

--- 02_go/new-q2.go
+++ 02_go/new-q2.go
@@ -3,30 +3,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 var (
-	MAX_CONSUMERS   = 10
-	count_consumers = 0
+	//MaxConsumers: maximo numero de consumidores
+	MaxConsumers   = 10
+	countConsumers = 0
 )
 
 func producer(ch chan<- int, n int, done chan<- bool) {
-	for i := 0; i < count_consumers; i++ {
+	for i := 0; i < countConsumers; i++ {
 		ch <- n
 		fmt.Println("Produced: ", n)
 	}
 	done <- true
-	close(ch)
+	//close(ch)
 }
 
-func consumer(id int, ch <-chan int, done chan<- bool) {
+func consumer(id int, ch <-chan int, done chan<- bool, wg *sync.WaitGroup) {
 	fmt.Println("Consumer ID:", id)
 	for {
 		select {
 		case n := <-ch:
 			fmt.Println("Consumer", id, "received:", n)
-			done <- true
+			wg.Done()
 			break
 		default:
 			fmt.Println("Consumer", id, "waiting producer")
@@ -37,19 +39,21 @@ func consumer(id int, ch <-chan int, done chan<- bool) {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	count_consumers = rand.Intn(MAX_CONSUMERS) + 1
-	count_consumers = 5
-	fmt.Println(count_consumers, " consumers")
+	//countConsumers = rand.Intn(MaxConsumers) + 1
+	countConsumers = 5
+	fmt.Println(countConsumers, " consumers")
 
-	ch := make(chan int, count_consumers)
+	ch := make(chan int, countConsumers)
 	done := make(chan bool)
 
 	rand.Seed(time.Now().UnixNano())
 
 	go producer(ch, rand.Intn(100), done)
-
-	for i := 0; i <= count_consumers; i++ {
-		go consumer(i, ch, done)
+	var wg sync.WaitGroup
+	wg.Add(countConsumers)
+	for i := 0; i < countConsumers; i++ {
+		go consumer(i, ch, done, &wg)
 	}
+	wg.Wait()
 	<-done
 }
