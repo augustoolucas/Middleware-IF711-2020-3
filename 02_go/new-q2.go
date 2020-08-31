c0a34c252cd9926a2f9b134dc33ca490ab401576
Lucas <lama2@cin.ufpe.br>
2020-08-30 22:55:27 -0300

    q2 done

--- 02_go/new-q2.go
+++ 02_go/new-q2.go
@@ -13,23 +13,21 @@ var (
 	countConsumers = 0
 )
 
-func producer(ch chan<- int, n int, done chan<- bool) {
+func producer(ch chan<- int, n int) {
 	for i := 0; i < countConsumers; i++ {
 		ch <- n
 		fmt.Println("Produced: ", n)
 	}
-	done <- true
-	//close(ch)
 }
 
-func consumer(id int, ch <-chan int, done chan<- bool, wg *sync.WaitGroup) {
+func consumer(id int, ch <-chan int, wg *sync.WaitGroup) {
 	fmt.Println("Consumer ID:", id)
 	for {
 		select {
 		case n := <-ch:
 			fmt.Println("Consumer", id, "received:", n)
 			wg.Done()
-			break
+			return
 		default:
 			fmt.Println("Consumer", id, "waiting producer")
 			time.Sleep(10 * time.Millisecond)
@@ -39,21 +37,19 @@ func consumer(id int, ch <-chan int, done chan<- bool, wg *sync.WaitGroup) {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	//countConsumers = rand.Intn(MaxConsumers) + 1
-	countConsumers = 5
+	countConsumers = rand.Intn(MaxConsumers) + 1
 	fmt.Println(countConsumers, " consumers")
+	time.Sleep(time.Second)
 
 	ch := make(chan int, countConsumers)
-	done := make(chan bool)
 
 	rand.Seed(time.Now().UnixNano())
 
-	go producer(ch, rand.Intn(100), done)
+	go producer(ch, rand.Intn(100))
 	var wg sync.WaitGroup
 	wg.Add(countConsumers)
 	for i := 0; i < countConsumers; i++ {
-		go consumer(i, ch, done, &wg)
+		go consumer(i, ch, &wg)
 	}
 	wg.Wait()
-	<-done
 }
