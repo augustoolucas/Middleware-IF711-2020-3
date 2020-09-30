ef34134753c1dba9d9174ca080583e6906a09be8
Lucas <lama2@cin.ufpe.br>
2020-09-29 23:13:49 -0300

    done

--- l3/cliente/cliente.go
+++ l3/cliente/cliente.go
@@ -3,15 +3,17 @@ package main
 import (
 	"calculadora/grpc/calculadora"
 	"fmt"
+	"github.com/gonum/stat"
 	"shared"
 	"strconv"
+	"sync"
 	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
-func main() {
+func CalculatorClientRPC(clientID int, wg *sync.WaitGroup) {
 	var idx int32
 
 	// Estabelece conexão com o server
@@ -22,21 +24,47 @@ func main() {
 	calc := calculadora.NewCalculatorClient(conn)
 
 	// contacta o server
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
+	defer wg.Done()
+
+	var responseTimes []float64
+	var totalTime time.Duration
 
 	for idx = 0; idx < shared.SAMPLE_SIZE; idx++ {
+		start := time.Now()
 
-		t1 := time.Now()
 		// invoca operação remota
-		rep, msgErr := calc.Add(ctx, &calculadora.Request{Op: "add", P1: idx, P2: idx})
+		_, msgErr := calc.Add(ctx, &calculadora.Request{Op: "add", P1: idx, P2: idx})
 
 		if msgErr != nil {
 			fmt.Println(idx, msgErr)
-		} else {
-			x := float64(time.Since(t1))
-
-			fmt.Printf("%f %d\n", x, rep.N)
 		}
+
+		//fmt.Printf("%f %d\n", x, rep.N)
+
+		executionTime := time.Since(start)
+		responseTimes = append(responseTimes, float64(executionTime))
+		totalTime += executionTime
 	}
+
+	meanFloat, stdDevFloat := stat.MeanStdDev(responseTimes, nil)
+	mean := time.Duration(meanFloat)
+	stdDev := time.Duration(stdDevFloat)
+	fmt.Println("ID: ", clientID, "Total time: ", totalTime, "- Mean: ", mean,
+		" - Standard Deviation: ", stdDev)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	start := time.Now()
+
+	for i := 0; i < shared.CLIENTS; i++ {
+		wg.Add(1)
+		go CalculatorClientRPC(i, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Total execution time: ", time.Since(start))
 }
