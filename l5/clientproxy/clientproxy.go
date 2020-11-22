3ea4034809f6f86f1222813241cd718abb374ace
vdcn1 <veetsirrah@gmail.com>
2020-11-22 18:45:43 -0300

    client proxy e lookup feitos

--- l5/clientproxy/clientproxy.go
+++ l5/clientproxy/clientproxy.go
@@ -1,5 +1,10 @@
 package clientproxy
 
+import (
+	"Middleware-IF711-2020-3/l5/hashing"
+	"fmt"
+)
+
 type ClientProxy struct {
 	Host     string
 	Port     int
@@ -7,8 +12,20 @@ type ClientProxy struct {
 	TypeName string
 }
 
+type Invocation struct {
+	Host    string
+	Port    int
+	Request hashing.Request
+}
+
 func (proxy ClientProxy) Hash(rawString string) string {
 	params := make([]interface{}, 1)
 	params[0] = rawString
+	request := hashing.Request{PwRaw: rawString}
+
+	invok := Invocation{proxy.Host, proxy.Port, request}
+
+	fmt.Println("Antes da invocacao: ", invok)
+	//da pro invoker o invok e retorna com a resposta do cliente
 	return ""
 }
